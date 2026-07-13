aced78c831077d767e2f825fc280366f6fe0cddc
Naivery <romeosinside@gmail.com>
2026-07-13 17:04:09 +0300

    implement controller tests

--- src/test/java/com/softserve/itacademy/controller/ToDoControllerTest.java
+++ src/test/java/com/softserve/itacademy/controller/ToDoControllerTest.java
@@ -1,4 +1,233 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.dto.todoDto.CreateToDoDto;
+import com.softserve.itacademy.dto.todoDto.ToDoDtoConverter;
+import com.softserve.itacademy.model.ToDo;
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.ToDoService;
+import com.softserve.itacademy.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ToDoController.class)
 public class ToDoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ToDoService todoService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private ToDoDtoConverter todoDtoConverter;
+
+    @Test
+    void createToDoForm_ShouldReturnCreateToDoView() throws Exception {
+        mockMvc.perform(get("/todos/create/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create-todo"))
+                .andExpect(model().attributeExists("todo"))
+                .andExpect(model().attribute("ownerId", 1L));
+    }
+
+    @Test
+    void createToDo_ShouldRedirect_WhenValid() throws Exception {
+        User owner = new User();
+        owner.setId(1L);
+        ToDo todo = new ToDo();
+        todo.setId(5L);
+
+        when(userService.readById(1L)).thenReturn(owner);
+        when(todoDtoConverter.toEntity(any(CreateToDoDto.class), any(User.class))).thenReturn(todo);
+
+        mockMvc.perform(post("/todos/create/users/1")
+                        .param("title", "New ToDo Submission"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/todos/all/users/1"));
+
+        verify(todoService).create(any(ToDo.class));
+    }
+
+    @Test
+    void createToDo_ShouldReturnForm_WhenValidationFails() throws Exception {
+        mockMvc.perform(post("/todos/create/users/1")
+                        .param("title", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create-todo"))
+                .andExpect(model().attribute("ownerId", 1L));
+
+        verify(todoService, never()).create(any());
+    }
+
+    @Test
+    void createToDo_ShouldReturnForm_WhenIllegalArgumentExceptionThrown() throws Exception {
+        User owner = new User();
+        owner.setId(1L);
+        ToDo todo = new ToDo();
+
+        when(userService.readById(1L)).thenReturn(owner);
+        when(todoDtoConverter.toEntity(any(CreateToDoDto.class), any(User.class))).thenReturn(todo);
+        when(todoService.create(any(ToDo.class))).thenThrow(new IllegalArgumentException("Title already exists"));
+
+        mockMvc.perform(post("/todos/create/users/1")
+                        .param("title", "Duplicate Title"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create-todo"))
+                .andExpect(model().attribute("ownerId", 1L))
+                .andExpect(model().hasErrors());
+    }
+
+    @Test
+    void updateToDoForm_ShouldReturnUpdateToDoView() throws Exception {
+        User owner = new User();
+        owner.setId(1L);
+        ToDo todo = new ToDo();
+        todo.setId(2L);
+        todo.setTitle("Old Title");
+        todo.setOwner(owner);
+
+        when(todoService.readById(2L)).thenReturn(todo);
+
+        mockMvc.perform(get("/todos/2/update/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("update-todo"))
+                .andExpect(model().attributeExists("todo"));
+    }
+
+    @Test
+    void updateToDo_ShouldRedirect_WhenValid() throws Exception {
+        User owner = new User();
+        owner.setId(1L);
+        ToDo todo = new ToDo();
+
+        when(todoService.readById(2L)).thenReturn(todo);
+        when(userService.readById(1L)).thenReturn(owner);
+
+        mockMvc.perform(post("/todos/2/update/users/1")
+                        .param("id", "2")
+                        .param("title", "Updated ToDo Title")
+                        .param("ownerId", "1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/todos/all/users/1"));
+
+        verify(todoService).update(any(ToDo.class));
+    }
+
+    @Test
+    void updateToDo_ShouldReturnForm_WhenValidationFails() throws Exception {
+        mockMvc.perform(post("/todos/2/update/users/1")
+                        .param("title", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("update-todo"));
+
+        verify(todoService, never()).update(any());
+    }
+
+    @Test
+    void updateToDo_ShouldReturnForm_WhenIllegalArgumentExceptionThrown() throws Exception {
+        User owner = new User();
+        owner.setId(1L);
+        ToDo todo = new ToDo();
+
+        when(todoService.readById(2L)).thenReturn(todo);
+        when(userService.readById(1L)).thenReturn(owner);
+        when(todoService.update(any(ToDo.class))).thenThrow(new IllegalArgumentException("Title match clash"));
+
+        mockMvc.perform(post("/todos/2/update/users/1")
+                        .param("id", "2")
+                        .param("title", "Clashing Title")
+                        .param("ownerId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("update-todo"))
+                .andExpect(model().hasErrors());
+    }
+
+    @Test
+    void delete_ShouldRedirect() throws Exception {
+        mockMvc.perform(get("/todos/2/delete/users/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/todos/all/users/1"));
+
+        verify(todoService).delete(2L);
+    }
+
+    @Test
+    void getAll_ShouldReturnTodosUserView() throws Exception {
+        User user = new User();
+        user.setId(1L);
+
+        when(todoService.getByUserId(1L)).thenReturn(new ArrayList<>());
+        when(userService.readById(1L)).thenReturn(user);
+
+        mockMvc.perform(get("/todos/all/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("todos-user"))
+                .andExpect(model().attributeExists("todos"))
+                .andExpect(model().attribute("user", user));
+    }
+
+    @Test
+    void getTasks_ShouldReturnTodoTasksView() throws Exception {
+        User owner = new User();
+        owner.setId(1L);
+        ToDo todo = new ToDo();
+        todo.setId(2L);
+        todo.setOwner(owner);
+        todo.setCollaborators(new java.util.HashSet<>());
+        todo.setTasks(new HashSet<>());
+
+        when(todoService.readById(2L)).thenReturn(todo);
+        when(userService.getAll()).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(get("/todos/2/tasks"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("todo-tasks"))
+                .andExpect(model().attribute("todo", todo))
+                .andExpect(model().attributeExists("tasks"))
+                .andExpect(model().attributeExists("users"));
+    }
+
+    @Test
+    void addCollaborator_ShouldRedirect() throws Exception {
+        mockMvc.perform(get("/todos/2/add").param("user_id", "3"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/todos/2/tasks"));
+
+        verify(todoService).addCollaborator(2L, 3L);
+    }
+
+    @Test
+    void removeCollaborator_ShouldRedirect() throws Exception {
+        mockMvc.perform(get("/todos/2/remove").param("user_id", "3"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/todos/2/tasks"));
+
+        verify(todoService).removeCollaborator(2L, 3L);
+    }
+
+    @Test
+    void shouldHandleEntityNotFoundException() throws Exception {
+        when(todoService.readById(999L)).thenThrow(new EntityNotFoundException("ToDo not found"));
+
+        mockMvc.perform(get("/todos/999/tasks"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("error/404"))
+                .andExpect(model().attributeExists("message"));
+    }
 }
--- src/test/java/com/softserve/itacademy/controller/UserControllerTest.java
+++ src/test/java/com/softserve/itacademy/controller/UserControllerTest.java
@@ -1,4 +1,170 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.dto.userDto.CreateUserDto;
+import com.softserve.itacademy.dto.userDto.UpdateUserDto;
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.model.UserRole;
+import com.softserve.itacademy.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void createGet_ShouldReturnCreateUserView() throws Exception {
+        mockMvc.perform(get("/users/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create-user"))
+                .andExpect(model().attributeExists("user"));
+    }
+
+    @Test
+    void createPost_ShouldRedirect_WhenValid() throws Exception {
+        User registeredUser = new User();
+        registeredUser.setId(1L);
+        registeredUser.setFirstName("John");
+
+        when(userService.register(any(CreateUserDto.class))).thenReturn(registeredUser);
+
+        mockMvc.perform(post("/users/create")
+                        .param("firstName", "John")
+                        .param("lastName", "Doe")
+                        .param("email", "john@mail.com")
+                        .param("password", "123456"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/todos/all/users/1"));
+
+        verify(userService).register(any(CreateUserDto.class));
+    }
+
+    @Test
+    void createPost_ShouldReturnForm_WhenValidationFails() throws Exception {
+        mockMvc.perform(post("/users/create")
+                        .param("firstName", "")
+                        .param("email", "invalid-email"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create-user"));
+
+        verify(userService, never()).register(any());
+    }
+
+    @Test
+    void createPost_ShouldReturnForm_WhenIllegalArgumentExceptionThrown() throws Exception {
+        when(userService.register(any(CreateUserDto.class)))
+                .thenThrow(new IllegalArgumentException("User with emailAlreadyExists@mail.com already exists"));
+
+        mockMvc.perform(post("/users/create")
+                        .param("firstName", "John")
+                        .param("lastName", "Doe")
+                        .param("email", "AlreadyExists@mail.com")
+                        .param("password", "123456"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create-user"))
+                .andExpect(model().hasErrors());
+    }
+
+    @Test
+    void read_ShouldReturnUserInfoView() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+
+        when(userService.readById(1L)).thenReturn(user);
+
+        mockMvc.perform(get("/users/1/read"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("user-info"))
+                .andExpect(model().attribute("user", user));
+    }
+
+    @Test
+    void updateGet_ShouldReturnUpdateUserView() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setEmail("john@mail.com");
+        user.setRole(UserRole.USER);
+
+        when(userService.readById(1L)).thenReturn(user);
+
+        mockMvc.perform(get("/users/1/update"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("update-user"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(model().attributeExists("roles"));
+    }
+
+    @Test
+    void updatePost_ShouldRedirect_WhenValid() throws Exception {
+        mockMvc.perform(post("/users/1/update")
+                        .param("firstName", "JohnUpdated")
+                        .param("lastName", "DoeUpdated")
+                        .param("email", "john@mail.com")
+                        .param("role", "USER"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/all"));
+
+        verify(userService).update(any(UpdateUserDto.class));
+    }
+
+    @Test
+    void updatePost_ShouldReturnForm_WhenValidationFails() throws Exception {
+        mockMvc.perform(post("/users/1/update")
+                        .param("firstName", "")
+                        .param("email", "invalid"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("update-user"))
+                .andExpect(model().attributeExists("roles"));
+
+        verify(userService, never()).update(any());
+    }
+
+    @Test
+    void delete_ShouldRedirect() throws Exception {
+        mockMvc.perform(get("/users/1/delete"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/all"));
+
+        verify(userService).delete(1L);
+    }
+
+    @Test
+    void getAll_ShouldReturnUsersListView() throws Exception {
+        when(userService.getAll()).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(get("/users/all"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("users-list"))
+                .andExpect(model().attributeExists("users"));
+    }
+
+    @Test
+    void shouldHandleEntityNotFoundException() throws Exception {
+        when(userService.readById(999L)).thenThrow(new EntityNotFoundException("User with id 999 not found"));
+
+        mockMvc.perform(get("/users/999/read"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("error/404"))
+                .andExpect(model().attributeExists("message"));
+    }
 }
